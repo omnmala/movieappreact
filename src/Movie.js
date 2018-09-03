d727e713c7669d44e82713c06db408326666e0b5
omnmala <omnmala@gmail.com>
2018-09-03 20:05:04 +0900

    update genre synopsis

--- src/Movie.js
+++ src/Movie.js
@@ -2,11 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({title, poster}) {
+function Movie({title, poster, genres, synopsis}) {
   return (
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
+    <div className="Movie">
+      <div className="Movie__Columns">
+        <MoviePoster poster={poster} />
+      </div>
+      <div className="Movie__Columns">
+        <h1>{title}</h1>
+        <div className="Movie__Genres">
+          <MovieGenres genres={genres} />
+        </div>
+        <div className="Movie__Synopsis">
+          <p>{synopsis}</p>
+        </div>
+      </div>
     </div>
   )
 }
@@ -17,13 +27,25 @@ function MoviePoster({poster}) {
   )
 }
 
+function MovieGenres({genres}) {
+  return (
+    <span className="Movie__Genre">{genres}</span>
+  )
+}
+
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
   poster: PropTypes.string.isRequired
+}
+
+MovieGenres.propTypes = {
+  genres: PropTypes.array.isRequired
 }
 
 export default Movie;
